aeaa796a9467000e8560b8222bcfd9450a3183d0
Kinga Wojtkowska <kinga.wolynska@gmail.com>
2020-08-26 11:36:58 +0200

    Add different solution with property

--- main.py
+++ main.py
@@ -9,23 +9,25 @@ class BaseContact:
         return ("{} {} {} {}".format(self.name, self.surname, self.phone, self.email))
 
     def contact(self, name, surname):
-        contact = [(i.name + ' ' + i.surname) for i in address if (name == i.name and surname == i.surname)]
+        contact = [(i.name, i.surname, i.contact_phone) for i in address if (name == i.name and surname == i.surname)]
         if len(contact) == 0:
-                txt = "Osoba o podanych danych nie widnieje w Twojej książce adresowej"
-                return txt
-        for i in address:
-            if (i.name + ' ' + i.surname) in contact:
-                txt = "Wybieram numer {} i dzwonię do {} {}".format(i.contact_phone, i.name, i.surname)
-                return txt
-            else:
-                pass
+            txt = "Osoba o podanych danych nie widnieje w Twojej książce adresowej"
+            return txt
+        else:
+            txt = "Wybieram numer {} i dzwonię do {} {}".format(contact[0][2], contact[0][0], contact[0][1])
+            return txt
+    
+    @property
+    def contact_phone(self):
+        x = input("Czy chcesz wybrać numer prywatny (P) czy biznesowy (B)? ")
+        if x == 'P':
+            return self.phone
+        elif x == 'B':
+            return self.bus_phone
+
     @property
     def label_length(self):
         return len(self.name) + len(self.surname) + 1
-
-    @property
-    def contact_phone(self):
-        return self.phone
                 
 class BusinessContact(BaseContact):
     def __init__(self, company, occupation, bus_phone, *args):
@@ -36,10 +38,16 @@ class BusinessContact(BaseContact):
 
     def __str__(self):
         return ("{} {} {} {} {}, {} {}".format(self.name, self.surname, self.phone, self.email, self.company, self.occupation, self.bus_phone))
-    
-    @property
-    def contact_phone(self):
-        return self.bus_phone
+
+def P_or_B_phone():
+    x = input("Czy chcesz wybrać numer prywatny (P) czy biznesowy (B)? ")
+    if x == 'P':
+        contact_phone = 'phone'
+        return contact_phone
+    elif x == 'B':
+        contact_phone = 'bus_phone'
+        return contact_phone
+
 
 def create_contacts(classid, number):
     from faker import Faker
@@ -61,7 +69,7 @@ address.append(BusinessContact('Friendly Advice', 'Claims investigator', '+48-73
 address.append(BusinessContact('Kleinhans', 'Telecommunications line installer', '+48-535-5575-13', 'Apolonia', 'Sokołowska', '+48-725-5599-16', 'ApoloniaSokolowska@teleworm.us'))
 address.append(BusinessContact('Jafco', 'Film processing technician', '+48-725-5528-72', 'Wiesław', 'Pawlak', '+48-575-5556-26', 'WieslawPawlak@rhyta.com'))
 address.append(BusinessContact('Laneco', 'University dean', '+48-695-5550-72', 'Dobrosława', 'Dudek', '+48-725-5572-12', 'DobroslawaDudek@armyspy.com'))
-address.append(BusinessContact('Ribs to Eat', 'Quality controller', '+48-665-5536-82', 'Jowita', 'Gmurek', '+48-455-5599-75', 'JowitaGmurek@rte.com'))
+address.append(BaseContact('Jowita', 'Gmurek', '+48-455-5599-75', 'JowitaGmurek@rte.com'))
 
 by_name = sorted(address, key=lambda person: person.name)
 by_surname = sorted(address, key=lambda person: person.surname)
